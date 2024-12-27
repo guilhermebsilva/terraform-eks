624c97987999a6962a07780e7754c5f0d59230a4
guilhermebsilva <guims08@gmail.com>
2024-12-27 16:00:33 -0300

    feat: added controller-module

--- provider.tf
+++ provider.tf
@@ -4,7 +4,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.80.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.35.0"
+    }
   }
+
+
   backend "s3" {
     bucket = "backend-tf-projeto-eks"
     key    = "dev/terraform.tfstate"
@@ -14,4 +20,14 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
 }
